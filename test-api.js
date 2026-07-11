b32454d9e892d42fd7eefc37047ddab88243b0a1
Rihen <rihenww@gmail.com>
2026-07-12 04:11:08 +0530

    deleted unwanted files

--- test-api.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-(async () => {
-  try {
-    const res = await axios.get('http://localhost:5001/api/interactions/offers');
-    console.log(res.status);
-  } catch (err) {
-    console.error(err.message, err.response ? err.response.status : '');
-  }
-})();
